5c49c3f813e0ab271ce494b769996c7327093042
Christian Carey <christianrcarey@gmail.com>
2016-06-15 14:45:50 -0400

    Refactor counting game.

--- counting_game.rb
+++ counting_game.rb
@@ -1,34 +1,47 @@
-def counting_game(players, max_number)
-  count = 1
-  person = 1
-  direction = :up
+class Counting_game
+  attr_accessor :count, :person, :direction, :players, :direction
+ 
+  def initialize(players = 10, max_number = 100)
+    @players = players
+    @max_number = max_number
+    @count = 1
+    @person = 1
+    @direction = :up
+  end
 
-  until count == max_number + 1
-    puts "count: #{count}"
-    puts "person: #{person}"
+  def switch_direction
+    @direction == :up ? @direction = :down : @direction = :up
+  end
 
-    if count % 7 == 0
-      direction == :up ? direction = :down : direction = :up
-    end
-
-    if direction == :up
-      if person == players
-        person = 0
+  def next_person
+    if @direction == :up
+      if @person == @players
+        @person = 1
       else 
-        person += 1
+        @person += 1
       end
-
-      person += 1 if count % 11 == 0
-    else 
-      if person == 0
-        person = players
+    else
+      if @person == 1
+        @person = @players
       else
-        person -= 1
+        @person -= 1
+      end
+    end
+  end
+
+  def run
+    until @count == @max_number + 1
+      puts "count: #{@count}"
+      puts "person: #{@person}"
+
+      if @count % 7 == 0
+        switch_direction
       end
 
-      person -= 1 if count % 11 == 0
-    end
+      next_person if @count % 11 == 0
+      next_person
 
-    count += 1
+      @count += 1
+    end
   end
 end
